2561de8dd7c22792df528c7036250077c1ead0a8
A <a@localhost>
2018-05-14 13:13:12 +0800

    Public transit bus stops processing added

--- vissim_v8/geo_math.py
+++ vissim_v8/geo_math.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def offsetParallel(points, distance, clockwise=True):
@@ -17,8 +18,14 @@ def offsetParallel(points, distance, clockwise=True):
             b[:, 0] = -norm[:, 1]
             b[:, 1] = norm[:, 0]
         return b
-    A = np.array(points)
+
+    tmp = np.array(points)
+    A=tmp.astype(np.float)
+    #print points	
+    #print "A is",A	
     B = np.vstack([-A[1], A[0], A[1:-1]])
+    #print "B is",B
+    #print "A-B is", A-B 	
     return np.array(perp(A-B, distance) + A, dtype='str')
 
 
@@ -70,3 +77,74 @@ def bezier(origin, destination, n):
         return [origin[1], destination[0]]
     else:
         return zip(Bx, By, Bz)
+
+
+def inRange(p,q,r):
+    """
+	return true iff q is between p and r
+    """
+    return ( (p <=q <= r) or (r<=q<=p) )
+
+
+def getDistance(point1, point2):
+    """ 
+	point1 and point2 to be x,y,z coordinates
+	Returns the length of the segment between the two points
+    """
+    tmp1= np.array(point1)
+    x1,y1,z1 = tmp1.astype(np.float)
+    tmp2= np.array(point2)
+    x2,y2,z2 = tmp2.astype(np.float)
+    d = (x2-x1) **2 + (y2-y1)** 2 + (z2-z1)**2
+    dist = math.sqrt(d)
+    return dist
+
+
+def getNearestPointOnLine(point1, line):
+    """
+	point1 is a given point
+	line is rep by 2 points
+	find the point on line where perp drawn from point1 meets the line
+    """
+    # first convert line to normalized unit vector
+    ptA = (np.array(line[0])).astype(np.float)
+    ptB = (np.array(line[1])).astype(np.float)
+    x1,y1,z1 = ptA
+    x2,y2,z2 = ptB
+    #
+    sepVec = ptA - ptB
+    x3,y3,z3 = (np.array(point1).astype(np.float))
+    mag = np.sqrt(sepVec.dot(sepVec))
+    dx=sepVec[0]
+    dy=sepVec[1]
+    dz=sepVec[2]
+    dx = dx/mag
+    dy = dy/mag
+    dz = dz/mag
+    # translate the point and get the dot product
+    lambda1 = dx * (x3 - x1) 
+    lambda2 = dy * (y3 - y1)
+    lambda3 = dz * (z3 - z1)
+    lam = lambda1 + lambda2 + lambda3
+    x4 = (dx * lam) + x1;
+    y4 = (dy * lam) + y1;
+    z4 = (dz * lam) + z1;
+    return x4,y4,z4
+
+def getNearestPointOnSegment(point1, segment):
+    """
+	point1 is a given point
+	segment is rep by 2 points
+	find the point on segment where perp drawn from point1 meets the segment
+	if the nearest point lies outside the segment, return nan
+    """
+    ptA = (np.array(segment[0])).astype(np.float)
+    ptB = (np.array(segment[1])).astype(np.float)
+    x1,y1,z1 = ptA
+    x2,y2,z2 = ptB
+    x3,y3,z3 =  getNearestPointOnLine(point1, segment)
+    if (inRange(x1,x3,x2) and inRange(y1,y3,y2) ) : # ignoring z 
+    	return (x3,y3,z3)
+    else:
+	#print 'perp falls outside the segment'
+	return None
